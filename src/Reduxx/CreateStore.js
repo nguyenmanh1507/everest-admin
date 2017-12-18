1d4f04a9968c6a87c4bf64e2093142bc2330fd12
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2017-12-18 16:55:45 +0700

    :construction: intergrate redux-saga

--- src/Reduxx/CreateStore.js
+++ src/Reduxx/CreateStore.js
@@ -1,9 +1,12 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 // creates the store
 export default rootReducer => {
   return createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(applyMiddleware(createSagaMiddleware))
   )
 }
--- src/Screens/OverviewScreen/OverviewScreen.js
+++ src/Screens/OverviewScreen/OverviewScreen.js
@@ -1,10 +1,110 @@
 // @flow
 
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { truncate, filter } from 'lodash'
+import moment from 'moment'
 
-class OverviewScreen extends Component<{}> {
+import { db } from 'FirebaseConfig'
+
+type State = {
+  data: Array<Object>
+}
+
+class OverviewScreen extends Component<{}, State> {
+  state = {
+    data: []
+  }
+
+  unsubscribeDataChange = null
+
+  getProducts = () => {
+    this.unsubscribeDataChange = db
+      .collection('products')
+      .orderBy('timestamp', 'asc')
+      .onSnapshot(
+        snapshot => {
+          if (!snapshot.metadata.hasPendingWrites) {
+            let newData = []
+
+            snapshot.docChanges.forEach(change => {
+              if (change.type === 'added') {
+                newData = [
+                  { id: change.doc.id, ...change.doc.data() },
+                  ...newData
+                ]
+              }
+
+              if (change.type === 'modified') {
+                newData = this.state.data.map(d => {
+                  if (d.id === change.doc.id) {
+                    return {
+                      id: change.doc.id,
+                      ...change.doc.data()
+                    }
+                  }
+
+                  return d
+                })
+              }
+
+              if (change.type === 'removed') {
+                newData = filter(this.state.data, d => d.id !== change.doc.id)
+              }
+            })
+            this.setState(({ data }) => ({
+              data: [...newData]
+            }))
+          }
+        },
+        error => {
+          throw Error(`Get products error: ${error}`)
+        }
+      )
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  componentWillUnmount() {
+    if (this.unsubscribeDataChange) {
+      this.unsubscribeDataChange()
+      console.log('unscrible data change')
+    }
+  }
+
   render() {
-    return <h1>Overview</h1>
+    const { data } = this.state
+
+    return (
+      <Fragment>
+        <h1>Overview</h1>
+        <table className="table bg-white">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Description</th>
+              <th scope="col">Price</th>
+              <th scope="col">Creat/Update</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((d, index) => (
+              <tr key={d.id} className="small">
+                <th scope="row">{index + 1}</th>
+                <td>{truncate(d.name, { length: 20, seperator: /,? +/ })}</td>
+                <td>
+                  {truncate(d.description, { length: 40, seperator: /,? +/ })}
+                </td>
+                <td>{d.price}</td>
+                <td>{moment(d.timestamp).fromNow()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
+    )
   }
 }
 
